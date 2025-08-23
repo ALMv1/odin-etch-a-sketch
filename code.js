aff6c7af9ad14587aa222e3a5e95dc97304eac09
ALMv1 <70581577+ALMv1@users.noreply.github.com>
2025-08-23 10:08:50 +0200

    add grid

--- code.js
+++ code.js
@@ -1,3 +1,24 @@
-function generateSquares{
-
+function generateSquares(side) {
+    for (let i = 0; i < side; i++) {
+        const vertical = document.createElement("div");
+        vertical.setAttribute("class", "vertical");
+        grid.appendChild(vertical);
+        for (let i = 0; i < side; i++) {
+            const horizontal = document.createElement("div");
+            horizontal.setAttribute("class", "horizontal");
+            vertical.appendChild(horizontal)
+        }
+    }
 }
+
+let side = 16;
+
+const container = document.createElement("div");
+container.setAttribute("class", "container")
+document.body.appendChild(container)
+
+const grid = document.createElement("div");
+grid.setAttribute("class", "grid");
+container.appendChild(grid);
+
+generateSquares(side);
