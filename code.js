dbd1c8d888053686bb4e23ba777b7c4667cc4b8a
ALMv1 <70581577+ALMv1@users.noreply.github.com>
2025-08-23 11:27:21 +0200

    change side to side and add button logic

--- code.js
+++ code.js
@@ -1,26 +1,52 @@
-function generateSquares(side) {
-    for (let i = 0; i < side; i++) {
+function generateGrid(size) {
+    for (let i = 0; i < size; i++) {
         const vertical = document.createElement("div");
         vertical.setAttribute("class", "vertical");
         grid.appendChild(vertical);
-        for (let i = 0; i < side; i++) {
+        for (let i = 0; i < size; i++) {
             const horizontal = document.createElement("div");
             horizontal.setAttribute("class", "horizontal");
-            vertical.appendChild(horizontal)
+            vertical.appendChild(horizontal);
             horizontal.addEventListener("mouseover", () => horizontal.style.backgroundColor = color);
         }
     }
 }
 
-let color = "red"
-let side = 16;
+let color = "red";
+let size = 16;
 
 const container = document.createElement("div");
-container.setAttribute("class", "container")
-document.body.appendChild(container)
+container.setAttribute("class", "container");
+
 
 const grid = document.createElement("div");
 grid.setAttribute("class", "grid");
+
+
+const ui = document.createElement("div");
+ui.setAttribute("class", "panel");
+
+const title = document.createElement("h1");
+title.setAttribute("class", "title");
+title.textContent = "Etch-a-sketch";
+
+const options = document.createElement("div");
+options.setAttribute("class", "options");
+
+const sizeBtn = document.createElement("button");
+sizeBtn.setAttribute("class", "button");
+sizeBtn.addEventListener("click", () => {
+    size = prompt("Enter a number between 0-100:");
+    while (size > 100 ){
+        size = prompt("Invalid number. Please enter a number between 0-100");
+    }
+    generateGrid(size);
+})
+
+document.body.appendChild(ui);
+ui.appendChild(title);
+ui.appendChild(options);
+document.body.appendChild(container);
 container.appendChild(grid);
 
-generateSquares(side);
+generateGrid(size);
